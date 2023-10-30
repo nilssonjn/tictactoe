d589e47085301a8e3d5774c278ba64ca716c1dc7
jnnilsson <jn.jensnilsson@gmail.com>
2023-10-30 17:28:31 +0100

    add playersymbols when clicking and buttonsCantBePressed after being clicked

--- src/main/java/com/example/tictactoe/HelloController.java
+++ src/main/java/com/example/tictactoe/HelloController.java
@@ -3,12 +3,20 @@ package com.example.tictactoe;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HelloController {
     private Model model = new Model();
 
     public void initialize() {
+        buttons = new ArrayList<>(Arrays.asList(button0, button1, button2, button3, button4, button5, button6, button7, button8));
+        for (Button button : buttons) {
+            buttonsCantBePressed(button);
+            button.setFocusTraversable(false);
+        }
+
     }
 
     @FXML
@@ -30,26 +38,45 @@ public class HelloController {
     @FXML
     private Button button8;
 
+    private int playerTurn = 0;
+    ArrayList<Button> buttons = new ArrayList<>();
+
     @FXML
-    private void restartGame(ActionEvent event){
+    private void restartGame(ActionEvent event) {
         //code for restarting game
     }
-    public void resetButton(Button button){
+
+    public void resetButton(Button button) {
         //code for resetting button, checking that button cant be pressed again
     }
-    public void checkWin(){
+
+    public void checkWin() {
         //code for checking if someone has won
     }
-    public void buttonsCantBePressed(){
-        //code for making buttons unpressable
+
+    public void buttonsCantBePressed(Button button) {
+        button.setOnMouseClicked(mouseEvent -> {
+            setPlayerSymbol(button);
+            button.setDisable(true);
+            checkWin();
+        });
     }
-    public void playerTurn(){
+
+    public void playerTurn() {
         //code for player turn
     }
-    public void computerTurn(){
+
+    public void computerTurn() {
         //code for computer turn
     }
-    public void setPlayerSymbol(){
-        //code for setting player symbol
+
+    public void setPlayerSymbol(Button button) {
+        if (playerTurn % 2 == 0) {
+            button.setText("X");
+            playerTurn = 1;
+        } else {
+            button.setText("O");
+            playerTurn = 0;
+        }
     }
 }
