daad587329a2bb0708e46265650976a3afb95b8d
jnnilsson <jn.jensnilsson@gmail.com>
2023-10-30 12:38:13 +0100

    add buttonid and method for restartgame

--- src/main/java/com/example/tictactoe/HelloController.java
+++ src/main/java/com/example/tictactoe/HelloController.java
@@ -1,18 +1,37 @@
 package com.example.tictactoe;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
-
     private Model model = new Model();
+
+    public void initialize() {
+    }
 
     @FXML
     private Button button0;
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+    @FXML
+    private Button button3;
+    @FXML
+    private Button button4;
+    @FXML
+    private Button button5;
+    @FXML
+    private Button button6;
+    @FXML
+    private Button button7;
+    @FXML
+    private Button button8;
 
-    public void initialize() {
-        welcomeText.textProperty().bind(model.messageProperty()); //binding the text visible in label
+    @FXML
+    private void restartGame(ActionEvent event){
+        //code for restarting game
     }
-
 }
